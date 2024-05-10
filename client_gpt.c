a1833836b6da849a648d9dc8abd2b0b8969b5283
kodai <kodaiadvance@icloud.com>
2024-05-10 12:04:51 +0900

    GPTプログラミングを追加

---
+++ client_gpt.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    int sock = 0;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // サーバーのIPアドレスを設定
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    // サーバーに接続
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // メッセージを送信し、サーバーの応答を待つ
+    send(sock, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+
+    // ソケットを閉じる
+    close(sock);
+    return 0;
+}
